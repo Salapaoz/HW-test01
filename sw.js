6e56a2951ba75b2c365953c44818bb0594ae333a
Salapaoz <phaskrcihli874@gmail.com>
2026-01-20 21:07:20 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,3 +7,10 @@ self.addEventListener("fetch", e => {
     fetch(e.request).catch(() => new Response("Offline"))
   );
 });
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("hw-v1").then(c =>
+      c.addAll(["./", "index.html", "style.css", "app.js"])
+    )
+  );
+});

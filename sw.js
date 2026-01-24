550fa1890431932300c3d53a0cb6553d71237feb
Salapaoz <phaskrcihli874@gmail.com>
2026-01-24 22:05:06 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,46 @@
-const CACHE_NAME = "hw-v1";
+const CACHE_NAME = "hw-v2";
+
 const ASSETS = [
   "/HW-test01/",
   "/HW-test01/index.html",
   "/HW-test01/style.css",
   "/HW-test01/app.js",
   "/HW-test01/manifest.json",
-  "/HW-test01/icons/icon-192.png",
-  "/HW-test01/icons/icon-512.png"
+  "/HW-test01/icon-192.png",
+  "/HW-test01/icon-512.png"
 ];
 
+// install
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
   );
   self.skipWaiting();
 });
 
+// activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(res => {
-      return res || fetch(event.request);
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
-
